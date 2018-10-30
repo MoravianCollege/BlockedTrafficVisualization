9d1a85c93f69feda7d8d74cfda29450aef325146
Manasseh Edwards <stmde03@moravian.edu>
2018-10-30 10:14:20 -0400

    Add files via upload

---
+++ UDP_Stream.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+# Create a TCP/IP socket for receiving data
+RCV_UDP_IP = "10.230.1.59"
+RCV_UDP_PORT = 5140
+
+# Bind the socket to the port
+rcv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+rcv_sock.bind((RCV_UDP_IP, RCV_UDP_PORT))
+
+while True:
+    # Receive message
+    data, address = rcv_sock.recvfrom(1024)
+
+    # Parse original message and create a new one
+    syslogmsg = data.split(",")
+    print >> sys.stderr, syslogmsg
