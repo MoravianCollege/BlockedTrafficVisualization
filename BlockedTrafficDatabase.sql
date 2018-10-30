f3b875ea7afdbde4081c66a8025a0cac298d2236
Zachary Balga <43012855+ZacharyB19@users.noreply.github.com>
2018-10-30 13:17:52 -0400

    revision for sql schema

--- BlockedTrafficDatabase.sql
+++ BlockedTrafficDatabase.sql
@@ -1,14 +1,16 @@
 # Recreate the database
 CREATE DATABASE BlockedTrafficDatabase;
 
+# For quick dropping
+DROP DATABASE BlockedTrafficDatabase;
+
 # Selecting the database for BlockedTraffic
-USE BlockedTraffic;
+USE BlockedTrafficDatabase;
 
 # Setup for each of the tables inside the BlockedTraffic database
 
 CREATE TABLE traffic(
-  data        VARCHAR(200) NOT NULL,
-  ipAddress   VARCHAR(50) NOT NULL,
-
-  PRIMARY KEY (ipAddress)
-);
+  timeOf VARCHAR(50) NOT NULL,
+  protocol VARCHAR(50) NOT NULL,
+  address VARCHAR(50) NOT NULL,
+  PRIMARY KEY (address));
